fa6352838bd52eb69c3cb0de805fac3b6a0f3abc
Ketan Shah <ketan.shah@gmail.com>
2019-03-19 08:47:46 +0530

    removed license check from activation hook

--- src/Activation.php
+++ src/Activation.php
@@ -5,19 +5,21 @@ namespace DF\Opentable;
 /**
  * Activation class.
  */
-class Activation {
+class Activation
+{
 
-	protected $container;
+    protected $container;
 
-	public function __construct($container) {
-		$this->container = $container;
-	}
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
 
-	/**
-	 * Plugin activation.
-	 */
-	public function install() {
-		$this->container['license']->init(); //License init while activating.
-		flush_rewrite_rules();
-	}
+    /**
+     * Plugin activation.
+     */
+    public function install()
+    {
+        flush_rewrite_rules();
+    }
 }
